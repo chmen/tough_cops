7ede215ed89911ad1f8d705a3de93b061b5d49da
chmen <tony.potapov@gmail.com>
2019-09-26 11:31:06 +0300

    add basic component for Posts

--- recordstore-front/src/components/Posts.vue
+++ recordstore-front/src/components/Posts.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="max-w-md m-auto py-10">
     <hr class="border border-grey-light my-6" />
+
+    <ul>
+      <li v-for="post in posts">
+        {{ post.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,8 +15,18 @@ export default {
   name: 'Posts',
   data() {
     return {
-      records: [],
+      posts: [],
       error: []
+    }
+  },
+  created() {
+    this.$http.secured.get('api/v1/posts')
+      .then(response => {this.posts = response.data})
+      .catch(error => this.setError(error, 'Something went wrong'))
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
     }
   }
 }
